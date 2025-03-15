5e3e53fea42d6b195461ff9b417c2280b29953d7
feybrown4 <owhuonkem2006@gmail.com>
2025-03-15 09:30:29 -0300

    CHORE: Added inventory class model without relationship

--- backend/elitecars-admin/obj/Debug/net9.0/elitecars-admin.AssemblyInfo.cs
+++ backend/elitecars-admin/obj/Debug/net9.0/elitecars-admin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elitecars-admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+339703522fbab700a4a8648735d16731315d6929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc78fa14c4501dfcbc56f1b3a5e658a06bb20a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("elitecars-admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elitecars-admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
