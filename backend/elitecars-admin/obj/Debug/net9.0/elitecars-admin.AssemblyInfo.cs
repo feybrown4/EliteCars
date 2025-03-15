3bd0588e96c1cf74dfbb63da0b7b7df6f49dd361
goldenhub <goldenazubuike@gmail.com>
2025-03-15 19:17:37 +0100

    update

--- backend/elitecars-admin/obj/Debug/net9.0/elitecars-admin.AssemblyInfo.cs
+++ backend/elitecars-admin/obj/Debug/net9.0/elitecars-admin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elitecars-admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c56f8f10a71020049c15742093c1e098b176ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb0ada997e23ba86fda7601eba7b7e8e660d74f")]
 [assembly: System.Reflection.AssemblyProductAttribute("elitecars-admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elitecars-admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
