29bbb3278505bde43bcb3e08d891e7f6f22a5455
feybrown4 <owhuonkem2006@gmail.com>
2025-03-15 09:10:35 -0300

    CHORE: Added car mod class model without relationship

--- backend/elitecars-admin/obj/Debug/net9.0/elitecars-admin.AssemblyInfo.cs
+++ backend/elitecars-admin/obj/Debug/net9.0/elitecars-admin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elitecars-admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4815bc5c0acb4481ccef5a0f8d4503e020ce163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+491b59e158c6bc537ba883d2dfd4f595a5da3e95")]
 [assembly: System.Reflection.AssemblyProductAttribute("elitecars-admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elitecars-admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
