0b206c125e76c3b1938f22429ab23becaceb43b8
goldenhub <goldenazubuike@gmail.com>
2025-03-19 16:05:21 +0100

    update: update

--- backend/elitecars-admin/Data/ApplicationDbContext.cs
+++ backend/elitecars-admin/Data/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 using elitecars_admin.Models;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace elitecars_admin.Data;
 
 public class ApplicationDbContext : DbContext
@@ -24,39 +23,42 @@ public class ApplicationDbContext : DbContext
     public DbSet<Role> Roles { get; set; } = null!;
     public DbSet<RolePermission> RolePermissions { get; set; } = null!;
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) {
-
-        modelBuilder.Entity<RolePermission>()
-                //composite primary key
-                .HasKey(rp => new { rp.RolePermissionId, rp.RoleId, rp.PermissionId });
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<RolePermission>()
+            //composite primary key
+            .HasKey(rp => new
+            {
+                rp.RolePermissionId,
+                rp.RoleId,
+                rp.PermissionId,
+            });
 
         // relationship with role
 
 
         // relationship with role
 
-        modelBuilder.Entity<CarMod>()
-            .HasKey(cm => new { cm.CarModId, cm.CarId } );
-        modelBuilder.Entity<CarMod>()
+        modelBuilder.Entity<CarMod>().HasKey(cm => new { cm.CarModId, cm.CarId });
+        modelBuilder
+            .Entity<CarMod>()
             .HasOne(c => c.Car)
             .WithMany(cm => cm.CarMods)
             .HasForeignKey(c => c.CarId);
 
-        modelBuilder.Entity<CarOption>()
-            .HasKey(co => new { co.CarOptionId, co.CarId }); 
-        modelBuilder.Entity<CarOption>()
+        modelBuilder.Entity<CarOption>().HasKey(co => new { co.CarOptionId, co.CarId });
+        modelBuilder
+            .Entity<CarOption>()
             .HasOne(c => c.Car)
             .WithMany(co => co.CarOptions)
             .HasForeignKey(c => c.CarId);
 
-        modelBuilder.Entity<Favorite>()
-            .HasKey(f => new { f.FavoriteId, f.CarId });
-        modelBuilder.Entity<Favorite>()
+        modelBuilder.Entity<Favorite>().HasKey(f => new { f.FavoriteId, f.CarId });
+        modelBuilder
+            .Entity<Favorite>()
             .HasOne(c => c.Car)
             .WithMany(f => f.Favorites)
             .HasForeignKey(c => c.CarId);
-
-
-
     }
 }
